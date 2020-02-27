bed5ba47f1cdabe3b5df44584a5b630f51dcd5df
clemenks <clemenks@mail.uc.edu>
2020-02-26 20:35:52 -0500

    Added ability to use camera

--- app/src/main/java/edu/uc/clemenks/keepingupwithschool/MainActivity.kt
+++ app/src/main/java/edu/uc/clemenks/keepingupwithschool/MainActivity.kt
@@ -1,10 +1,20 @@
 package edu.uc.clemenks.keepingupwithschool
 
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.ImageView
+import android.widget.Toast
 import edu.uc.clemenks.keepingupwithschool.ui.main.MainFragment
+import kotlinx.android.synthetic.main.main_activity.*
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainActivity : AppCompatActivity() {
+
+    val CAMERA_REQUEST_CODE = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +24,28 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+        btnTakePic.setOnClickListener {
+            //Defines the Camera Application
+            val callCameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if(callCameraIntent.resolveActivity(packageManager) != null){
+                startActivityForResult(callCameraIntent, CAMERA_REQUEST_CODE)
+            }
+        }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when(requestCode){
+            CAMERA_REQUEST_CODE-> {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    imageView.setImageBitmap(data.extras.get("data") as Bitmap)
+                }
+            }
+            else ->{
+                Toast.makeText(this, "Unreconized request code", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
 }
