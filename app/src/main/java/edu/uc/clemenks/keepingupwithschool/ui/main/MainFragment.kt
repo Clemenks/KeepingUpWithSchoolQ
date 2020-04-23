6444223d826aa37d9bdd72c2de00eba6a075d2b6
Xavier Baszuk <48223045+baszukxj@users.noreply.github.com>
2020-04-23 16:26:16 -0400

    Add firebase integration

--- app/src/main/java/edu/uc/clemenks/keepingupwithschool/ui/main/MainFragment.kt
+++ app/src/main/java/edu/uc/clemenks/keepingupwithschool/ui/main/MainFragment.kt
@@ -2,16 +2,15 @@ package edu.uc.clemenks.keepingupwithschool.ui.main
 
 import android.Manifest
 import android.app.Activity.RESULT_OK
-import android.content.Context
-import android.content.ContextWrapper
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.Uri
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.os.Build
 import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -19,19 +18,38 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.core.view.drawToBitmap
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.UserInfo
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
+import com.google.firebase.storage.FirebaseStorage
 import edu.uc.clemenks.keepingupwithschool.R
 import kotlinx.android.synthetic.main.main_fragment.*
 
+
+class PhotoToBeSaved(val course: String, val imageName: String, var imageRefURI: String, var documentId: String) {}
+
 class MainFragment : Fragment() {
 
-    val CAMERA_PERMISSION_REQUEST_CODE = 1999
-    val CAMERA_REQUEST_CODE = 1998
+    private val AUTH_REQUEST_CODE = 2002
+    private val CAMERA_PERMISSION_REQUEST_CODE = 1999
+    private val CAMERA_REQUEST_CODE = 1998
+    private lateinit var viewModel: MainViewModel
+    private var firestore = FirebaseFirestore.getInstance()
+    private var storageReference = FirebaseStorage.getInstance().getReference()
+    private var user : FirebaseUser? = null
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+
+
+    init {
+        firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().build()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -47,11 +65,24 @@ class MainFragment : Fragment() {
            prepTakePhoto()
        }
        btnSave.setOnClickListener{
-           saveImageToInternalStorage(imgPhotoView.drawToBitmap() as Bitmap)
+           var URI = saveImageToInternalStorage(imgPhotoView.drawToBitmap() as Bitmap)
+           saveImageToFireStore(URI.toString())
            Toast.makeText(context, "Photo Saved", Toast.LENGTH_LONG).show()
        }
+        btnLogon.setOnClickListener {
+            logon()
+        }
 
     }
+
+    private fun logon() {
+        var providers = arrayListOf(AuthUI.IdpConfig.EmailBuilder().build())
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(providers).build(),
+            AUTH_REQUEST_CODE
+        )
+    }
+
     //See if we have permission or not
     private fun prepTakePhoto() {
         if(ContextCompat.checkSelfPermission(context!!, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
@@ -102,6 +133,9 @@ class MainFragment : Fragment() {
                 val imageBitmap = data!!.extras!!.get("data") as Bitmap
                 imgPhotoView.setImageBitmap(imageBitmap)
             }
+            else if(requestCode == AUTH_REQUEST_CODE){
+                user = FirebaseAuth.getInstance().currentUser
+            }
         }
     }
 
@@ -122,7 +156,68 @@ class MainFragment : Fragment() {
         return Uri.parse(savedImageURL)
     }
 
+    private fun saveImageToFireStore(photoURI: String) {
 
+        if(user == null){
+            Toast.makeText(context, "Please press the power button to login to save.", Toast.LENGTH_LONG).show()
+            return
+        }
+
+        user ?: return
+
+        var photoInfo = PhotoToBeSaved(actClassName.text.toString(), txtImgName.text.toString(), "", "")
+
+        val document  = firestore.collection("images").document()
+        val set = document.set(photoInfo)
+            set.addOnSuccessListener {
+                Log.d("Firebase", "Saved.")
+                photoInfo.documentId = document.id
+                uploadPhoto(photoURI, photoInfo)
+            }
+            set.addOnCanceledListener {
+                Log.d("Firebase", "Failed")
+            }
+    }
+
+    private fun uploadPhoto(
+        photoURI: String,
+        photoInfo: PhotoToBeSaved
+
+    ) {
+
+        var uri = Uri.parse(photoURI)
+        val imageRef = storageReference.child("images/"  + (user?.uid ?: return ) + "/" + uri.lastPathSegment)
+        val uploadTask = imageRef.putFile(uri)
+
+        uploadTask.addOnSuccessListener {
+
+            val downloadUrl = imageRef.downloadUrl
+            downloadUrl.addOnSuccessListener {
+
+                photoInfo.imageRefURI = it.toString()
+
+                //update firestore
+                updatePhotoMetaData(photoInfo)
+            }
+        }
+        uploadTask.addOnFailureListener{
+
+            Log.e(TAG, it.message)
+        }
+
+    }
+
+    private fun updatePhotoMetaData(photoInfo: PhotoToBeSaved) {
+        firestore.collection("images" )
+            .document(photoInfo.documentId)
+            .set(photoInfo)
+            .addOnSuccessListener {
+                Log.d("Firestore", "Complete")
+            }
+            .addOnFailureListener{
+                Log.e("Firestore", "Failure")
+            }
+    }
 
 
 }
